c5b4e2c706cba62d7fcaa9424a7f6c2b02cb5387
lee <q2298478324@gmail.com>
2019-09-06 16:43:17 +0900

    first

---
+++ Assets/_Complete-Game/Scripts/Enemy/EnemyHealthUIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CompleteProject
+{
+    public class EnemyHealthUIManager : MonoBehaviour
+    {
+        private static EnemyHealthUIManager ins;
+        public static EnemyHealthUIManager Ins => ins;
+        public GameObject enemyHealthUI;
+        public Slider currentUI;
+        public Slider showUI;
+        public float showTime;
+        private EnemyHealth currentHealth;
+        private float current;
+        private float show;
+        private float timer;
+
+        void Awake()
+        {
+            ins = this;
+        }
+
+        public void Show(EnemyHealth health, float previous)
+        {
+            enemyHealthUI.SetActive(true);
+            timer = 0f;
+            show = previous;
+            current = health.currentHealth;
+            showUI.value = previous;
+            currentUI.value = current;
+        }
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer > showTime)
+            {
+                enemyHealthUI.SetActive(false);
+            }
+            else
+            {
+                show = Mathf.Lerp(current, show, 0.96f);
+                showUI.value = show;
+            }
+        }
+    }
+}
+
