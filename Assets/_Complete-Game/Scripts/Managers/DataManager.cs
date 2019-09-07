3427f246b978d8ed0f9fd3d2b2dacbd7c0c0e703
lee <q2298478324@gmail.com>
2019-09-07 09:25:02 +0900

    add life bar and creating bullet

---
+++ Assets/_Complete-Game/Scripts/Managers/DataManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : SingletonMonoBehaviour<DataManager>
+{
+    public GameObject bullet;
+    protected override void UnityAwake() { }
+
+}
---
+++ Assets/_Complete-Game/Scripts/Util/Singleton.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Singleton
+/// </summary>
+public abstract class Singleton<T>
+where T : Singleton<T>, new()
+{
+    private static T ins;
+    public static T Ins
+    {
+        get
+        {
+            if (ins == null)
+            {
+                ins = new T();
+            }
+            return ins;
+        }
+    }
+    public virtual void Destroy() => Reset();
+    public static void Reset() => ins = null;
+}
+
+/// <summary>
+/// Monobehaviour's singleton
+/// </summary>
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour
+     where T : SingletonMonoBehaviour<T>
+{
+    public static T Ins { get; protected set; }
+
+    protected abstract void UnityAwake();
+
+    private void Awake()
+    {
+        if (enabled == false)
+        {
+            return;
+        }
+        if (Ins == null)
+        {
+            //ゲーム開始時にGameManagerをinstanceに指定ß
+            Ins = this as T;
+            UnityAwake();
+        }
+        else if (Ins != this)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+        else
+        {
+            // Do Nothing
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (this == Ins) Ins = null;
+    }
+}
