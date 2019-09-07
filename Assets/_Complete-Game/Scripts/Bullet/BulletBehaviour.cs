20158c186f0e3a7188b4883e0cff11fb8b51abed
lee <q2298478324@gmail.com>
2019-09-07 09:58:24 +0900

    add bullet

---
+++ Assets/_Complete-Game/Scripts/Bullet/BulletBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    public class BulletBehaviour : MonoBehaviour
+    {
+        private int damagePerShot;
+        private Vector3 direction;
+        public float speed;
+        public void Initial(Vector3 start, Vector3 direction, int damagePerShot)
+        {
+            this.direction = direction.normalized;
+            this.damagePerShot = damagePerShot;
+        }
+
+        void Update()
+        {
+            this.transform.position += direction * Time.deltaTime * speed;
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Enemy")
+            {
+                EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+
+                // If the EnemyHealth component exist...
+                if (enemyHealth != null)
+                {
+                    // ... the enemy should take damage.
+                    enemyHealth.TakeDamage(damagePerShot, this.transform.position);
+                }
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
